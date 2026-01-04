84de8f0cf4aa8e2edf373b4b1d106d581c5492c7
yoshihiro satou <128808496+yoshihiro-satou@users.noreply.github.com>
2026-01-04 21:00:28 +0900

    redererまで

--- section4/main.js
+++ section4/main.js
@@ -1,14 +1,22 @@
-import * as THREE from'./build/three.module.js';
+import * as THREE from'../build/three.module.js';
 
-let scene, camera;
+let scene, camera, renderer;
 
-// 市０んを追加
+// シーンを追加
 scene = new THREE.Scene();
 
 // カメラを追加
-camera = new THREE.PerspectiveCamara(
+camera = new THREE.PerspectiveCamera(
   50,
-  windew.innerwidth / window.innerHeight,
+  window.innerWidth / window.innerHeight,
   0.1,
   1000
 )
+
+// レンダラーを追加
+renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
+
+
