33fe9712b271059588ca69363d9c2e503b58de39
yoshihiro satou <128808496+yoshihiro-satou@users.noreply.github.com>
2026-01-07 21:01:58 +0900

    next pointLight

---
+++ light-for-Udemy/main.js
@@ -0,0 +1,106 @@
+import * as THREE from "./build/three.module.js";
+import { OrbitControls } from "./controls/OrbitControls.js";
+
+//UIデバッグ
+const gui = new dat.GUI();
+
+//サイズ
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+//シーン
+const scene = new THREE.Scene();
+
+//カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.x = -2;
+camera.position.y = 1;
+camera.position.z = 4;
+scene.add(camera);
+
+//ライト
+const ambientLight = new THREE.AmbientLight();
+ambientLight.color = new THREE.Color(0xffffff);
+ambientLight.intensity = 0.5;
+// scene.add(ambientLight);
+gui.add(ambientLight, "intensity").min(0).max(1).step(0.001);
+
+const directionalLight =  new THREE.DirectionalLight(0xffffff, 0.5);
+// directionalLight.position.set(1, 0.55, 5);
+// scene.add(directionalLight);
+
+const hemiSphereLight = new THREE.HemisphereLight(0x0ffff0, 0xffff00, 1);
+hemiSphereLight.position.set(1, 0.55, 0);
+scene.add(hemiSphereLight)
+//マテリアル
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.3;
+
+//オブジェクト
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
+sphere.position.x = -1.5;
+
+const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+  material
+);
+torus.position.x = 1.5;
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
+plane.rotation.x = -Math.PI * 0.5;
+plane.position.y = -0.65;
+
+scene.add(sphere, cube, torus, plane);
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+//レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+document.body.appendChild(renderer.domElement);
+
+//コントロール
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const clock = new THREE.Clock();
+
+const animate = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = 0.1 * elapsedTime;
+  cube.rotation.y = 0.1 * elapsedTime;
+  torus.rotation.y = 0.1 * elapsedTime;
+
+  sphere.rotation.x = 0.15 * elapsedTime;
+  cube.rotation.x = 0.15 * elapsedTime;
+  torus.rotation.x = 0.15 * elapsedTime;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(animate);
+};
+
+animate();
--- threeJs-material-section/main.js
+++ threeJs-material-section/main.js
@@ -1,7 +1,7 @@
 import * as THREE from "./build/three.module.js";
 import { OrbitControls } from "./controls/OrbitControls.js";
 
-let scene, camera, renderer, pointLight, controls;
+let scene, camera, renderer, pointLight, controls, sphere, plane, octahedron;
 
 window.addEventListener("load", init);
 
@@ -28,6 +28,53 @@ function init() {
    * マテリアルセクション
    */
 
+  // ジオメトリ
+  const sphereGeometry = new THREE.SphereGeometry(0.5, 16, 16);
+  const planeGeometry =new THREE.PlaneGeometry(1, 1);
+  const octahedronGeometry = new THREE.OctahedronGeometry(0.5);
+
+  // テクスチャ
+  const texture = new THREE.TextureLoader().load('./textures/brick.jpg');
+  // マテリアル
+  // const material = new THREE.MeshBasicMaterial();
+  // material.map = texture;
+  // material.color = new THREE.Color("red");
+  // material.wireframe = true;
+  // material.side = THREE.DoubleSide;
+  // material.opacity = 0.5
+  // material.transparent = true //opacityをかけるとき
+
+  // const material = new THREE.MeshNormalMaterial()
+  // material.flatShading = true;
+
+  // const material = new THREE.MeshStandardMaterial();
+  // // material.color.set("#049ef4");
+  // material.roughness = 0.34;
+  // material.metalness = 0.64;
+  // material.map = texture;
+  const material = new THREE.MeshPhongMaterial();
+  material.shininess = 1000;
+  material.specular = new THREE.Color("red")
+  // 明かりを追加してみよう
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+  scene.add(ambientLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 0.3);
+  pointLight.position.set(1, 2, 3);
+  scene.add(pointLight)
+
+  const pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+scene.add(pointLightHelper)
+
+  // メッシュ化
+  sphere = new THREE.Mesh(sphereGeometry, material);
+  plane = new THREE.Mesh(planeGeometry, material);
+  octahedron = new THREE.Mesh(octahedronGeometry, material);
+
+  sphere.position.x = -1.5;
+  octahedron.position.x = 1.5;
+
+  scene.add(sphere, plane, octahedron);
   //マウス操作
   const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -35,8 +82,20 @@ function init() {
 
   animate();
 }
+const clock = new THREE.Clock();
 
 function animate() {
+  const elapsedTime = clock.getElapsedTime();
+// オブジェクトを回転させる
+sphere.rotation.x= elapsedTime;
+plane.rotation.x= elapsedTime;
+octahedron.rotation.x= elapsedTime;
+
+sphere.rotation.y = elapsedTime;
+plane.rotation.y = elapsedTime;
+octahedron.rotation.y = elapsedTime;
+
+
   //レンダリング
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
