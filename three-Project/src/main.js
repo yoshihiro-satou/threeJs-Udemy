37e524001cfedbb82ec7d9a11f880f61eacd327b
yoshihiro satou <128808496+yoshihiro-satou@users.noreply.github.com>
2026-01-11 21:00:41 +0900

    camera position

--- three-Project/src/main.js
+++ three-Project/src/main.js
@@ -1,5 +1,11 @@
 import './style.css'
 import * as THREE from "three";
+import * as dat from "lil-gui";
+
+/**
+ * UIデバックを実装
+ */
+const gui = new dat.GUI();
 
 /*
 * 必須の３要素を追加しよう
@@ -46,6 +52,10 @@ const material = new THREE.MeshPhysicalMaterial({
   flatShading: true,
 });
 
+gui.addColor(material, "color");
+gui.add(material, "metalness").min(0).max(1).step(0.001);
+gui.add(material, "roughness").min(0).max(1).step(0.001);
+
 // メッシュ
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
 const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material);
@@ -63,4 +73,85 @@ mesh3.position.set(2, 0, -6);
 mesh4.position.set(5, 0, 3);
 
 scene.add(mesh1, mesh2, mesh3, mesh4);
-renderer.render(scene, camera);
+const meshes = [mesh1, mesh2, mesh3, mesh4];
+// ライトを追加
+const directionalLight = new THREE.DirectionalLight("#ffffff", 4);
+directionalLight.position.set(0.5, 1, 0);
+scene.add(directionalLight);
+
+// ブラウザのリサイズ操作
+window.addEventListener("resize", () => {
+  // サイズのアップデート
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // カメラのアップデート
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // レンダラーのアップデート
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
+// ホイールを実装してみよう
+let speed = 0;
+let rotation = 0;
+window.addEventListener('wheel', (event) => {
+  speed += event.deltaY * 0.0002;
+  console.log(speed);
+
+});
+
+function rot() {
+  rotation += speed;
+  speed *= 0.93;
+
+  // ジオメトリ全体を回転させる
+  mesh1.position.x = 2 + 3.8 * Math.cos(rotation);
+  mesh1.position.z = -3 + 3.8 * Math.sin(rotation);
+  mesh2.position.x = 2 + 3.8 * Math.cos(rotation + Math.PI / 2);
+  mesh2.position.z = -3 + 3.8 * Math.sin(rotation + Math.PI / 2);
+  mesh3.position.x = 2 + 3.8 * Math.cos(rotation + Math.PI);
+  mesh3.position.z = -3 + 3.8 * Math.sin(rotation + Math.PI);
+  mesh4.position.x = 2 + 3.8 * Math.cos(rotation + 3 * (Math.PI / 2));
+  mesh4.position.z = -3 + 3.8 * Math.sin(rotation + 3 * (Math.PI / 2));
+
+  window.requestAnimationFrame(rot)
+}
+rot();
+
+// カーソルの位置を取得してみよう
+const cursor = {};
+cursor.x = 0;
+cursor.y = 0;
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+  console.log(cursor);
+});
+
+// アニメーション
+
+const clock = new THREE.Clock();
+
+const animate = () => {
+  renderer.render(scene, camera);
+
+let getDeltaTime = clock.getDelta();
+// console.log(getDeltaTime);
+
+// mesh回転させる
+for(const mesh of meshes) {
+  mesh.rotation.x += 0.1 * getDeltaTime;
+  mesh.rotation.y += 0.12 * getDeltaTime;
+}
+
+// カメラの制御しよう
+camera.position.x = cursor.x * getDeltaTime * 3;
+camera.position.y = -cursor.y * getDeltaTime * 3;
+
+
+  window.requestAnimationFrame(animate);
+}
+animate();
