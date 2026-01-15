646c9127a5453897ce9662e345be719af91da29d
yoshihiro satou <128808496+yoshihiro-satou@users.noreply.github.com>
2026-01-15 21:00:13 +0900

    complete threejsUdemy

--- realObject-threejs/src/main.js
+++ realObject-threejs/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 console.log(THREE);
 
@@ -25,6 +26,43 @@ const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(window.devicePixelRatio);
 
-renderer.render(scene, camera);
+// envimage
+const urls = [
+  "./envImage/right.png",
+  "./envImage/left.png",
+  "./envImage/up.png",
+  "./envImage/down.png",
+  "./envImage/front.png",
+  "./envImage/back.png",
+]
+
+const loader = new THREE.CubeTextureLoader();
+scene.background = loader.load(urls);
+
+//controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(500);
+
+//cubecamera
+const cubeCamera = new THREE.CubeCamera(1, 1000, cubeRenderTarget);
+scene.add(cubeCamera);
+// object
+const material = new THREE.MeshBasicMaterial({
+  envMap: cubeRenderTarget.texture,
+  reflectivity: 1,
+});
+const geometry = new THREE.SphereGeometry(350, 50, 50);
+const sphere = new THREE.Mesh(geometry, material);
+sphere.position.set(0, 100, 0);
+scene.add(sphere);
+function animate() {
+  controls.update();
+  cubeCamera.update( renderer, scene);
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate)
+}
+animate();
 
 
